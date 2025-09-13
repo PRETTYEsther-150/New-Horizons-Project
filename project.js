d5b5a8961260b32bc049a4a3a88be120381921d6
PRETTYEsther-150 <adelekeesther69@gmail.com>
2025-09-13 08:50:41 +0100

    Login functionality.

--- project.js
+++ project.js
@@ -6,15 +6,15 @@ errorMessage.style.fontFamily = 'Arial, sans-serif';
 passwordInput.parentNode.insertBefore(errorMessage, passwordInput.nextSibling);
 passwordInput.addEventListener('input', function() {
     const password = this.value;
-    const hasnumber = /[0-9]/.test(password);
-    const hassymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-    if (passwordInput.value.length < 8) {
+    if (password.length < 8) {
         errorMessage.textContent = 'Password must be at least 8 characters long.';
-    } else if (!hasnumber) {
+    } else if (!hasNumber) {
         errorMessage.textContent = 'Password must contain at least one number.';
     } else if (!hassymbol) {
-        errorMessage.textContent = 'Password must contain at least one special character.';
+            errorMessage.textContent = 'Password must contain at least one special character.';
         } else {
             errorMessage.textContent = '';
         }
